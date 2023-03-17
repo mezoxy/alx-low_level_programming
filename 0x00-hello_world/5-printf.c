62c5a860f4db4e079d1b2991dfb9e18e18da14d6
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-16 19:57:44 -0700

    Adding 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main : this is the main fuction as s starting point for program*
+ * return : ends the fuction and returns to the calling function
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return(0);
+}
