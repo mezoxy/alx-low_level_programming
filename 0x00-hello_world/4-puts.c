4ac15ca179e20053a7a46eee7770d6b8b0a0cf02
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-17 12:01:30 -0700

    Adding the first file in this dir

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
 /**
- * main - the entry point main function
- * return - always return o
+ * return - Stop the program
+ * main - Entry point
 */
 
 
-int main (void)
+int main(void)
 {
 	puts("\"Probramming is like building a multilingual puzzle");
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /*
- * main : this is the main fuction as s starting point for program*
+ * main : this is the main fuction as s starting point for program
  * return : ends the fuction and returns to the calling function
- */
-int main(void)
+*/
+
+int main (void)
 {
-printf (\"with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
