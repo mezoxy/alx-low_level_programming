bdd11b841bdf077fd512667552d77eb6d0f9d117
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-17 12:37:47 -0700

    Edditing the 4th file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/* return - Stop the program*/
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-	/* main(void) - Entry point */
 {
 	puts("\"Probramming is like building a multilingual puzzle");
 	return (0);
