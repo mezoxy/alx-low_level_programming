f3357cc241ea507e3ca4006d06c74e55ce326b64
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-18 11:28:41 -0700

    Replacing s with S

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(b));
-	printf("size of an int: %ld byte(s)\n", sizeof(a));
-	printf("size of a long int: %ld byte(s)\n", sizeof(c));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(d));
-	printf("size of a float: %ld byte(s)\n", sizeof(e));
+	printf("Size of a char: %ld byte(s)\n", sizeof(b));
+	printf("Size of an int: %ld byte(s)\n", sizeof(a));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
+	printf("Size of a float: %ld byte(s)\n", sizeof(e));
 	return (0);
 }
