88cb24c8ce40796f3857558b8e1fbc660dfea349
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-19 16:31:51 -0700

    Adding 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for ( ; ch <= 'z' ; ch++)
+	{
+	if (ch != 'e' && ch != 'q')
+	{
+		putchar(ch);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
