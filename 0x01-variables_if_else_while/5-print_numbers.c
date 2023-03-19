57598280399a8bd77e1154f71bb476537d63a97c
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-19 16:46:59 -0700

    Adding 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	for ( ; x <= 9 ; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
