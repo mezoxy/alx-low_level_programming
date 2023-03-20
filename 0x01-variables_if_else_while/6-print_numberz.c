2c1e2daf8642499da9278bf96747e52a6839bc5c
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-19 17:09:39 -0700

    Adding 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 48;
+
+	for ( ; x <= 57 ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
