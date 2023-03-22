34bdc56a7585399b4c7e65f894eb07439db6c66a
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-22 14:50:48 -0700

    Changing descreption

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - Printing the sign of a number
  *
- * Return: 1 if n greter than 0, 0 if n == 0, -1 if n less than zero
+ * Return: 1 if +,0 if 0 and -1 if -
  * @n: Input
  */
 
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar(45);
+		_putchar('-');
 		return (-1);
 	}
 }
