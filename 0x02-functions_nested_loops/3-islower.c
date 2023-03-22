6e591e30c7dba1ab04201e17653ab7c86de48a75
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-22 12:18:06 -0700

    Adding 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Function tht checks for lowercase character
+ *
+ * Return: 1 if the charcter is lowercase, 0 if it's not
+ *
+ * @c: The character to check
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
