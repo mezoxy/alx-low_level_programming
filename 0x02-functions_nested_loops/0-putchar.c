17660017597c5e10530fdda7ea838e475d1673ad
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-21 17:49:43 -0700

    Adding 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = '_', b = 'p', c = 'u', d = 't', e = 'c';
+	char f = 'h', g = 'a', h = 'r', i = '\n';
+
+	_putchar(a);
+	_putchar(b);
+	_putchar(c);
+	_putchar(d);
+	_putchar(e);
+	_putchar(f);
+	_putchar(g);
+	_putchar(h);
+	_putchar(i);
+	return (0);
+}
