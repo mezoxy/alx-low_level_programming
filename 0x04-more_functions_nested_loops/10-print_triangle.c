810b71c2583482a5b334fd537505333dc3fc11b8
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-23 16:32:05 -0700

    Adding 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - That prints a triangle
+ * @size: input
+ */
+void print_triangle(int size)
+{
+	int x;
+	int y;
+	int k;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = x; y <= size; y++)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= x; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
