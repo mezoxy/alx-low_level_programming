a53570b9531b65121c1dff7f6b1db87738fe6f36
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-22 19:53:01 -0700

    Edditing 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,9 +5,9 @@
 void jack_bauer(void)
 {
 	int h, m;
-	for (h ==0; h <= 23; h++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (m == 0;m <= 59; m++)
+		for (m = 0;m <= 59; m++)
 		{
 			_putchar((h / 10) + 48);
 			_putchar((h % 10) + 48);
