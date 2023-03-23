b4b551cf806ffcf1233d6484c67ef2afa4f9f2c8
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-22 17:00:38 -0700

    Adding 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit ofa number
+ * @n: Input
+ * Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int m = n % 10;
+
+	return (m);
+}
