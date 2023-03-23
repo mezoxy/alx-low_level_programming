c3b9c8baf6937beff2710d9fe65369e83673f178
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-22 18:35:48 -0700

    Adding 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Adds two integers
+ * @i: Input
+ * @j: Input
+ * Return: Result
+ */
+int add(int i, int j)
+{
+	int n = i + j;
+
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * jack_bauer - prints evry minute of the day
+ */
+void jack_bauer(void)
+{
+
+}
