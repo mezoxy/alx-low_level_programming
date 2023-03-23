8f1aacccd9d99545a05f1cb61d469477eaa04852
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-23 16:07:56 -0700

    Resolving the 7th task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - Draws a diagonal line
+ * @n: Input
+ */
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			for (y = 1; y <= x; y++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
