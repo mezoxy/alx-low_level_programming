0a192a6d1580d8d12363e7ec389a582ac9b6f1c9
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-23 13:55:41 -0700

    Adding 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - Function print a square
+ * @n: Input
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
