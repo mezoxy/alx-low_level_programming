37407a8cb5bb0f219ea907d4377f88c7aed59911
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-23 19:43:10 -0700

    Edditing the 5th file for the 3th time

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 	int x;
 	int y;
 
-	for (x = 1; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (y = 1; y <= 14; y++)
+		for (y = 0; y <= 14; y++)
 		{
 			if (y <= 9)
 			{
@@ -20,6 +20,6 @@ void more_numbers(void)
 				_putchar((y / 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
