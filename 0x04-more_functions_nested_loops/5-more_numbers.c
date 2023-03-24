877ed1868a6108b0220dfabd8c796c10110b57d8
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-23 19:53:06 -0700

    Changing the answer into the 5th file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,21 @@ void more_numbers(void)
 {
 	int x;
 	int y;
+	int z;
 
-	for (x = 0; x < 10; x++)
+	for (x = 1; x <= 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y <= 9)
+			z = y;
+			if (y > 9)
 			{
-				_putchar((y % 10) + '0');
+				_putchar(1 + 48);
+				z = y % 10;
 			}
 			else
 			{
-				_putchar((y / 10) + '0');
+				_putchar(z + 48);
 			}
 		}
 		_putchar('\n');
