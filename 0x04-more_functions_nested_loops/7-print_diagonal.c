512a707f61367cbf1b9128ba5ce3d4e220a98d4b
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-23 17:20:14 -0700

    Adding a condition

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,20 +12,21 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 	}
-	else if (n == 1)
-	{
-		_putchar('\\');
-		_putchar('\n');
-	}
 	else
 	{
 		for (x = 1; x <= n; x++)
 		{
 			for (y = 1; y <= x; y++)
 			{
+				if (y != x)
+				{
 				_putchar(' ');
+				}
+				else
+				{
+					_putchar(92);
+				}
 			}
-			_putchar(92);
 			_putchar('\n');
 		}
 	}
