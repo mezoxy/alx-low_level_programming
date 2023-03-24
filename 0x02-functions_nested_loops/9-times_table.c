3cd22d95cd15472b095f3ce0bfa25d9b050641a2
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-24 16:23:00 -0700

    Never give up

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,20 +6,24 @@ void times_table(void)
 {
 	int i;
 	int j;
-	int k = i * j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (k > 9)
+			if (i * j > 9)
 			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar(j * i / 10 + '0');
+				_putchar(j * i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
 			else
 			{
-				_putchar(k  + '0');
+				_putchar(j * i  + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
