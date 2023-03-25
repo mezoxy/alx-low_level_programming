ea952da09ae7d65eef07e25890a614a355d91bee
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-24 17:49:47 -0700

    Please be correct

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 		{
 			if (y / 10 != 0)
 			{
-				_putchar(y / 10 + 48);
+				_putchar(y / 10 + '0');
 			}
 			else
 			{
-				_putchar(y % 10 + 48);
+				_putchar(y % 10 + '0');
 			}
 		}
 		_putchar('\n');
