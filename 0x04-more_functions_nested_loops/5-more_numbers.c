169546b4ba7deaef3ce809bd0a9faf6b95ead253
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-24 17:46:15 -0700

    Never give up until I die

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,18 @@ void more_numbers(void)
 {
 	int x;
 	int y;
-	int z;
 
 	for (x = 1; x <= 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			z = y;
-			if (y > 9)
+			if (y / 10 != 0)
 			{
-				_putchar(1 + 48);
-				z = y % 10;
+				_putchar(y / 10 + 48);
 			}
 			else
 			{
-				_putchar(z + 48);
+				_putchar(y % 10 + 48);
 			}
 		}
 		_putchar('\n');
