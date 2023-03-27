6f6e86f548f52ad7645b9b3b0ac18a07b3e1304f
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-26 19:05:40 -0700

    Im not in mood

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,22 +6,21 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
 	}
-	else
+	else if (n < 98)
 	{
 		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
-		printf("98");
 	}
-	printf("\n");
+	printf("98\n");
 }
