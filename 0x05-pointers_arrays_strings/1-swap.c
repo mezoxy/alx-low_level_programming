3703bbbb8b8b9f0e0ae192e0c3e0fb9e5603ab88
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-27 07:09:44 -0700

    Adding 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - This function can swaps the values of two integers
+ * @a: Input 1
+ * @b: Input 2
+ */
+void swap_int(int *a, int *b)
+{
+	int *p = a;
+
+	*a = *p;
+	*b = *a;
+	*b = *p;
+
+
+}
