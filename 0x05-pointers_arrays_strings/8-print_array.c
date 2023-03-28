75245bf0b4ba71e09e570688f22aa64325e282ea
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-28 14:07:58 -0700

    Using for loop and adding new line

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,9 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i <= n - 1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d, ", a[i]);
 	}
-	_putchar('\n');
+	printf("%d\n", a[n -1]);
 }
