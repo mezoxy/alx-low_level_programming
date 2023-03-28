97d347a617fc1c4f3bef01fb24e7279437cf5389
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-28 15:15:46 -0700

    Adding the 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * char *_strcpy - Function copies the string pointed to by src
+ * @src: Pointer
+ * @dest: Pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i - 1; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[i] = '\0';
+}
