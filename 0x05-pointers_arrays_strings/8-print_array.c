dfb894b17d5e492622ba479d844c8cc83033fc68
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-28 14:15:15 -0700

    Adding the condition if n = 0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,16 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n - 1; i++)
+	if (n == 0)
 	{
-		printf("%d, ", a[i]);
+		printf("%d\n", a[0]);
 	}
-	printf("%d\n", a[n -1]);
+	else
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		printf("%d\n", a[n - 1]);
+	}
 }
