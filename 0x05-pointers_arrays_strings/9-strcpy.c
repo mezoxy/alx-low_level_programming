98764ed13919ab8706a09ccfeabeecbbd1bfc102
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-28 15:21:10 -0700

    Adding the return value

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,6 +3,7 @@
  * char *_strcpy - Function copies the string pointed to by src
  * @src: Pointer
  * @dest: Pointer
+ * Return: The pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -18,4 +19,5 @@ char *_strcpy(char *dest, char *src)
 		dest[j] = src[j];
 	}
 	dest[i] = '\0';
+	return (dest);
 }
