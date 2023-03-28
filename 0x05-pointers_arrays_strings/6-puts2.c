ac7b65c83165d0028a0da2b0ed628ba0c3ce9191
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-27 18:48:30 -0700

    Adding 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - Function that prints every other character of a string
+ * @str: Pointer
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (i = 0; j <= i; j++)
+	{
+		while (j % 2 == 0)
+		{
+			_putchar(str[j]);
+			j++;
+		}
+	}
+}
