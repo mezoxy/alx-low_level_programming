008de08e9c8bd9940606faf40f99ccbb72a616c7
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-30 16:20:04 -0700

    Adding conditions

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,15 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[0] = '\0';
 	}
-	else
+	while (i < n && src[i] != '\0')
 	{
-		while (i < n)
-		{
-			dest[i] = src[i];
-			i++;
-		}
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
