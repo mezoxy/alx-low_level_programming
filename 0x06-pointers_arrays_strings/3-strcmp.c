e54d14fd8bd88f144ba363d81fe3cc0cc1cbcf3c
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-31 16:46:08 -0700

    Chnging the code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int k;
-	int j = 0;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 
 		if (s1[i] != s2[i])
 		{
-			j = 1;
+			k = s1[i] - s2[i];
 			break;
 		}
-	}
-	if (k == 1)
-	{
-		k = s1[i] - s2[i];
-	}
-	else
-	{
-		k = 0;
 	}
 	return (k);
 }
