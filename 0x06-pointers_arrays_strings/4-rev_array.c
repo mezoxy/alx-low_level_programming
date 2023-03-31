45f1baab58ee7de37ccbfa8d36df6d80c3dee5df
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-30 17:42:58 -0700

    Adding condition if n odd

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,20 @@ void reverse_array(int *a, int n)
 	int i = 0;
 	char e;
 
-	for (i = 0; i <= n / 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
-		e = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = e;
+		if (n % 2)
+		{
+			e = a[i];
+			a[i] = a[n - i - 1];
+			a[n - i - 1] = e;
+		}
+		else
+		{
+			e = a[i];
+			a[i] = a[n - i - 1];
+			a[n - i - 1] = e;
+			a[n / 2] = a[n / 2];
+		}
 	}
 }
