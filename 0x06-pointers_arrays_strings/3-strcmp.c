d9b7313a6bc3826dc5c0bb9bbcb719ffcd380ecf
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-31 15:28:18 -0700

    Edditing the code for the 3th time

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,28 +1,23 @@
 #include "main.h"
 /**
  * _strcmp - Function that compares two strings
- * @s1: Pointer
- * @s2: Pointer
+ * @s1: String 1
+ * @s2: String 2
  * Return: 0 if they are equal
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 	int k;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (i = 0;s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		k == (s1[i] - s2[i]) + 0;
 
 		if (s1[i] != s2[i])
 		{
-			return (k);
+			k = s1[i] - s2[i];
 			break;
 		}
-		i++;
-		else
-		{
-			return (0);
-		}
 	}
+	return (k);
 }
