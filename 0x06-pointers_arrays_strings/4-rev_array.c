d23461c1d4f1d9abe027d0512053fb1bdbf4b21c
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-03-30 17:58:38 -0700

    Changing the code

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,20 +9,10 @@ void reverse_array(int *a, int n)
 	int i = 0;
 	char e;
 
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (n % 2)
-		{
-			e = a[i];
-			a[i] = a[n - i - 1];
-			a[n - i - 1] = e;
-		}
-		else
-		{
-			e = a[i];
-			a[i] = a[n - i - 1];
-			a[n - i - 1] = e;
-			a[n / 2] = a[n / 2];
-		}
+		e = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = e;
 	}
 }
