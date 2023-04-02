3421bd129970e550b4efb8ad58f533c2c3d16b21
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-02 07:39:50 -0700

    Deleting some conditions

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,6 +8,7 @@ char *cap_string(char *stg)
 {
 	int i;
 	int j = 0;
+	char spe[] = {',', ';', '\n', '\t', ')', '(', '{', '}', '.', ' ', '?', '!'};
 
 	while (stg[j] != '\0')
 	{
@@ -15,21 +16,13 @@ char *cap_string(char *stg)
 	}
 	for (i = 0; i < j; i++)
 	{
-		if (stg[i] == ' ' && stg[i + 1] >= 97 && stg[i + 1] <= 122 && i < j)
+		if (stg[i] == spe[i] && stg[i + 1] >= 97 && stg[i + 1] <= 122 && i < j)
 		{
 			stg[i + 1] = stg[i + 1] - 32;
 		}
 		else if (stg[0] >= 97 && stg[0] <= 122)
 		{
 			stg[0] = stg[0] - 32;
-		}
-		else if (stg[i] == '.' && stg[i + 1] >= 97 && stg[i + 1] <= 122)
-		{
-			stg[i + 1] = stg[i + 1] - 32;
-		}
-		else if (stg[i] == 92 && stg[i + 2] >= 97 && stg[i + 2] <= 122)
-		{
-			stg[i + 2] = stg[i + 2] - 32;
 		}
 	}
 	return (stg);
