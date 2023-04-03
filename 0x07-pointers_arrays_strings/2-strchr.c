c862be06ac2ca4d6980b3afe6ea8239f1f11cc3d
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-03 05:38:54 -0700

    Adding 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - a function that locates a character in string
+ * @s: String
+ * @c: Character to copy
+ * Return: The first position of c if is it not found return NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (0);
+}
