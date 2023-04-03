59171ed4429b4e2a17b3d37a98b93a478008bd86
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-03 04:39:36 -0700

    Changing for loop with while

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,13 +4,14 @@
  * @s: Destination
  * @n: Number of bytes
  * @b: Character that will be replaced
+ *
  * Return: Pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	while (i < n)
+	while (i < n && n > 0)
 	{
 		s[i] = b;
 		i++;
