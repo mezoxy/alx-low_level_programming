5c2b262a15110d19d4c42b5722d528c9c65910e6
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-03 10:12:36 -0700

    Deleting

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,7 @@ char *_strchr(char *s, char c)
 
 	for ( ; s[i] != '\0' || s[i] == '\0'; i++)
 	{
-		if (s[i] == c && s[i] != '\0')
-		{
-			return (s + i);
-		}
-		else
+		if (s[i] == c)
 		{
 			return (s + i);
 		}
