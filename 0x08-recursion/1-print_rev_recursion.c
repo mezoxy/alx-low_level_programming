a2b07458d95cf96e641da56b7d488d77d4feadd9
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-05 10:02:18 -0700

    Adding 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - A function that prints a string in revers
+ * @s: String to print in revers
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*(s - 1));
+	s--;
+}
