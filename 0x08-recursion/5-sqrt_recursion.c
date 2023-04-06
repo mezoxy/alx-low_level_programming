b53245ebbecde9fafe9a8dc05e5bdffbc0494b88
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-05 17:34:57 -0700

    Deleting s--

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - A function that calculate the square root of a number
+ * @n: Input (number)
+ * Return: Root of number
+ */
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+	int k = _pow_recursion(i, 2)
+
+	if (n < i * i)
+	{
+		return (-1);
+	}
+	if (n == i * i)
+	{
+		return (i);
+	}
+}
