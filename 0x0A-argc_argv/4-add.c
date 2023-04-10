0e0124f65524334ad73aea4b7a2dd56df57780ef
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-10 13:27:06 -0700

    Changing the if confition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < 47 || *argv[i] > 56)
+		if (*argv[i] < 48 || *argv[i] > 57)
 		{
 			printf("Error\n");
 			return (1);
