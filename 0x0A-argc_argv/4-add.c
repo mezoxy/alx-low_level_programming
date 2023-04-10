c4135e0ae27c06cb66523854dda6188f521b109b
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-10 12:07:21 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,32 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 /**
- *  * main - Entry point
- *   * Return: Always 0 (Success)
- *    * @argc: Number of arguments
- *     * @argv: Argument vector
- *      */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * @argc: Number of arguments
+ * @argv: Argument vector
+ */
 int main(int argc, char *argv[])
 {
 	int i;
 	int sum = 0;
 
-        for (i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
+		if (*argv[i] < 47 || *argv[i] > 56)
+		{
+			printf("Error\n");
+			return (1);
+		}
 		sum = sum + atoi(argv[i]);
 	}
-	if (argc > 1)
+	if (argc == 1)
 	{
-		printf("%d\n", sum);
+		printf("0\n");
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+		printf("%d\n", sum);
 	}
 	return (0);
 }
