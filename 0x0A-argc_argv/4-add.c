b6e2811ffc8bf0d005ad2d6325acd1414860758d
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-10 14:03:34 -0700

    Adding a pointer

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,10 +10,13 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int sum = 0;
+	char *c;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+		c = argv[i];
+
+		if (*c < '0' || *c > '9')
 		{
 			printf("Error\n");
 			return (1);
