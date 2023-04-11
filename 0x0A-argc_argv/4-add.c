865f9012217d65044f95720918ee9957d39a7a70
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-10 18:56:41 -0700

    Changing the condition for the 5th time

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,22 +10,21 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int j;
 	int sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+		if (!(isdigit(argv[i][j])))
 		{
 			printf("Error\n");
 			return (1);
 		}
 		sum = sum + atoi(argv[i]);
+		}
 	}
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else
 	printf("%d\n", sum);
 	return (0);
 }
