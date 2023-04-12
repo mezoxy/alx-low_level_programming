3a3ac23b91bf4d38f206e64ab9ba0c16e752e42f
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-12 15:29:59 -0700

    Adding 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <sdtlib.h>
+/**
+ * alloc_grid - A function that return a pointer
+ * Return: A pointer
+ * @width: Dimension 1 (nbr of rows)
+ * @height: Dimensoin 2 (nbr of columns)
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	ptr = (int **) malloc(sizeof(int *) * height);
+
+	if (!ptr)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = (int *) malloc(sizeof(int) * width);
+
+		if (!ptr[i])
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(ptr[j]);
+			}
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
