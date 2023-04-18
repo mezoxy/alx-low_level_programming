fd4abe163c31fdae4b966ce0b98debddf0dfa04f
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-18 14:59:22 -0700

    Adding the allocation for owner and name

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 /**
  * new_dog - A function that creat a new dog
  * Return: A pointer to dog_t
@@ -19,8 +20,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	ptr->name = name;
-	ptr->owner = owner;
+	ptr->name = malloc(sizeof(name) + 1);
+	ptr->owner = malloc(sizeof(owner) + 1);
+
+	if (!ptr->name || !ptr->owner)
+	{
+		free(name);
+		free(owner);
+		return (NULL);
+	}
+	strcpy(ptr->name, name);
+	strcpy(ptr->owner, owner);
 	ptr->age = age;
 	return (ptr);
 }
