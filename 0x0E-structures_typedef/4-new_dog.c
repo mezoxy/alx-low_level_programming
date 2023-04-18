9d769d44dea88af16d77bb8245ab62b054de54fe
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-18 15:02:37 -0700

    Add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,15 +20,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	ptr->name = malloc(sizeof(name) + 1);
-	ptr->owner = malloc(sizeof(owner) + 1);
-
-	if (!ptr->name || !ptr->owner)
-	{
-		free(name);
-		free(owner);
-		return (NULL);
-	}
 	strcpy(ptr->name, name);
 	strcpy(ptr->owner, owner);
 	ptr->age = age;
