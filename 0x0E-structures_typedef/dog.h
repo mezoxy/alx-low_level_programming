b56624fea614d76195c6b6d95504e196513ebcb4
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-17 17:24:10 -0700

    Adding dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOH_H
+#define DOH_H
+/**
+ * struct dog - A struct that containes nultiple members
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: The owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
