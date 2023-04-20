62081a87cf7e78b293db06d29ba0be83e3e31104
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-20 11:40:18 -0700

    adding the 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		int arg = va_arg(args, int);
 
-		if (separator != NULL && i < n -1)
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%d%c", arg, *separator);
 		}
