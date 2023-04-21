1ae9f81432c2d1dfdeb8b1ed039d3d3af9d99383
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-20 19:06:27 -0700

    does not work

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", pp);
 		}
-		if (separator != NULL && i < n - 1)
+		if (separator != NULL && i != n - 1)
 		{
 			printf("%s", separator);
 		}
