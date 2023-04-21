ac383434754dd24fb209e74383c4973293abd0bb
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-20 18:37:37 -0700

    Adding n == 0 and reduce the length of code

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,17 +27,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
-		if (separator != NULL && i < n - 1)
+		else
 		{
-			printf("%s%s", pp, separator);
-		}
-		else if (i == n - 1)
-		{
-			printf("%s", pp);
-		}
-		else if (separator == NULL)
-		{
-			printf("%s", pp);
+			printf("%s%s", pp, (separator != NULL && i < n - 1) ? separator : "");
 		}
 	}
 	printf("\n");
