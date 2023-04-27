349d23b8e3ba71d32edd81690fa7321c13288242
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-04-27 15:13:36 -0700

    Changing the code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,19 +13,21 @@ size_t print_list(const list_t *h)
 {
 	int i = 0;
 
-	if (h == NULL)
+	if (!h)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	while (h != NULL)
 	{
-		if (h->str == NULL)
+		if (!(h->str))
 		{
 			printf("[0] (nil)\n");
-			return (1);
 		}
+		else
+		{
 		printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		i++;
 	}
