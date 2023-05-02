a52476ea457ddfa69f519a197869c423feffba33
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-05-01 19:54:20 -0700

    ADDing 7th

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,9 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *ptr;
+	listint_t *ptr = head;
 	unsigned int i = 0;
 	unsigned int j = 0;
-	ptr = head;
 
 	while (ptr)
 	{
