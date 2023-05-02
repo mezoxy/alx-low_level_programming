36dafeb1b0b63295a501899b8c7620370fd44866
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-05-02 13:26:47 -0700

    v

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new = malloc(sizeof(listint_t));
 
-	if (!new || !(*head))
+	if (!new || !head)
 		return (NULL);
 
 	new->n = n;
