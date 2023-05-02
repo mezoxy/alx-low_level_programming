67ac7ce6209bd2d24550e5c5feb77b605e8287c4
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-05-01 19:47:50 -0700

    Adding 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,2 +1,22 @@
 #include "lists.h"
+/**
+ * sum_listint - A function that calculates sum
+ * @head: A pointer
+ * Return: Sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *ptr;
 
+	if (!head)
+		return (0);
+	ptr = head;
+	
+	while (ptr)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
+}
