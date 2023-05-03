a688933813dfaeeb3e54fcb16602fd80199c01f2
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-05-03 00:58:34 -0700

    FF

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,29 +8,27 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *ptr0;
-	listint_t *ptr1;
+	listint_t *ptr0 = head, *ptr1 = head;
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	ptr1 = head->next;
-
-	while (ptr1)
+	while (ptr1 && ptr0 && ptr1->next)
 	{
-		if (ptr1 == ptr1->next)
+		ptr1 = ptr1->next->next;
+		ptr0 = ptr0->next;
+
+		if (ptr1 == ptr0)
 		{
+			ptr0 = head;
+			while (ptr0 != ptr1)
+			{
+				ptr0 = ptr0->next;
+				ptr1 = ptr1->next;
+			}
 			return (ptr1);
 		}
-		for (ptr0 = head; ptr0 != NULL; ptr0 = ptr0->next)
-		{
-			if (ptr0 == ptr1->next)
-			{
-				return (ptr1->next);
-			}
-		}
-		ptr1 = ptr1->next;
 	}
-	return (head);
+	return (NULL);
 }
