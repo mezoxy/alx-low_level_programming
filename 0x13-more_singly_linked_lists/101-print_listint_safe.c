e6ebb31c0115054538932219bbee6f4118d530ba
AYOUB MEZHAR <ayoub.mezhar@usmba.ac.ma>
2023-05-03 00:22:53 -0700

    Add

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,4 +8,67 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
+	size_t i, j = 0;
+	i = nmbr_uniqu(head);
+
+	if (i == 0)
+	{
+		for (; head != NULL; i++)
+		{
+			printf("[%p] %d\n", (void *)head, (head->n));
+			head = head->next;
+		}
+	}
+	else
+	{
+		for (; j < i; j++)
+		{
+			printf("[%p] %d\n", (void *)head, (head->n));
+			head = head->next;
+		}
+		printf("->[%p] %d\n", (void *)head, (head->n));
+	}
+	return (i);
+}
+/**
+ * nmbr_uniqu - A function that counts
+ * @head: A pointer
+ * Return: 0 if it's not
+ */
+size_t nmbr_uniqu(const listint_t *head)
+{
+	const listint_t *t, *h;
+	size_t i = 1;
+
+	if (head == NULL || head->next == NULL)
+	{
+		return (0);
+	}
+	t = head->next;
+	h = head->next->next;
+
+	while (h)
+	{
+		if (t == h)
+		{
+			t = head;
+			while (t != h)
+			{
+				i++;
+				t = t->next;
+				h = h->next;
+			}
+			t = t->next;
+
+			while (t != h)
+			{
+				i++;
+				t = t->next;
+			}
+			return (i);
+		}
+		t = t->next;
+		h = h->next->next;
+	}
+	return (0);
 }
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -5,5 +5,32 @@
  * @head: A pointer
  * Return: The address of the node where the loop starts
  */
+
 listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *ptr0;
+	listint_t *ptr1;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	ptr1 = head->next;
+
+	while (ptr1)
+	{
+		if (ptr1 == ptr1->next)
+		{
+			return (ptr1);
+		}
+		for (ptr0 = head; ptr0 != NULL; ptr0 = ptr0->next)
+		{
+			if (ptr0 == ptr1->next)
+			{
+				return (ptr1->next);
+			}
+		}
+		ptr1 = ptr1->next;
+	}
+	return (head);
 }
