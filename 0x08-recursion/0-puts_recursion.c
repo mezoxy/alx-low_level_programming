981f401fa2fcddf146ccd0f2b7d2284198871b37
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-05 07:30:15 -0700

    Task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '')
+	if (*s == '\0')
 	{
-		_putchar('');
+		_putchar('\0');
 	}
 	else
 		_putchar(s[0]);
-	return (_puts_recursion(*(s + 1)));
+	_puts_recursion(s + 1);
 }
