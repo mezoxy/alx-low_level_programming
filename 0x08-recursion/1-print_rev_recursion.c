c1808d02b7f5fa3db92af65eda17faafe32b644d
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-05 18:41:05 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - A function
+ * @s: A string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
