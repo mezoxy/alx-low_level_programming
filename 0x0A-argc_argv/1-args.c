881a37c81a66f42a13a55dc848758ebbdd5c3950
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-07 13:40:57 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: Count of arguments
+ * @argv: An array of pointers
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+}
+	int i;
+
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
