b950b06579a36332f18e1732815dbbaf64f50b06
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-11 17:21:09 -0700

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * alloc_grid - A function
+ * @width: First dimensin
+ * @height: Second
+ * Return: A pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **ptr = malloc(sizeof(int *) * height);
+	int i, j;
+
+	if (width <= 0 || height <= 0 || !ptr)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+
+		if (!ptr[i])
+		{
+			return (NULL);
+		}
+		for (j = 0; j < height; j++)
+		{
+			ptr[i][j] = 0;
+		}
+
+	}
+	return (ptr);
+}
