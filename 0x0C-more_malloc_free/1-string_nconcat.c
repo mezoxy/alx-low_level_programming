f32c9b65830024502f523de8b1f5159501506c96
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-13 14:43:06 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (ptr);
 	}
 	else
+	{
 		strcpy(ptr, s1);
-	strncat(ptr, s2, n);
-	return (ptr);
+		strncat(ptr, s2, n);
+		return (ptr);
+	}
 }
