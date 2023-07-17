b04d5aab6bb740d654a1aabd5de6a61be676098b
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-17 14:42:17 -0700

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,8 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (!d)
-		return (NULL);
+	if (d == NULL)
+		return;
 	d->owner = owner;
 	d->name = name;
 	d->age = age;
