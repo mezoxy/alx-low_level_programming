a4b7b54bcd7070412b59c90b9df42d80084e4079
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-17 15:03:49 -0700

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,9 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
-	if (d->owner == NULL)
+	else if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	if (!(d->name))
+	else if (!(d->name))
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
