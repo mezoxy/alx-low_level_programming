9cc3805b6c5d7277a124d783c5fe1e651f811861
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-17 16:58:31 -0700

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ typedef struct dog
 	char *owner;
 	float age;
 } my_dog;
-#define dog_t my_dog
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
