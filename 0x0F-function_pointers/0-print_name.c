d7a038b9c2bf085451a494e2d2ad9c35453e5828
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-19 10:35:00 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * print_name - A function that prints a name
+ * @name: A string
+ * @f: A function pointer
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
