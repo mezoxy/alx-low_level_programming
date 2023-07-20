0d18263034d54e28cb03e2d946be1a8a3a59660d
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-19 19:31:16 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,7 +2,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main(void)
+#include "function_pointers.h"
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: A string
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
 {
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (!get_op_func(op))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
