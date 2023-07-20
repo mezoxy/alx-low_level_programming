652e3e7bc477acd9bad474082bf25a74a7032d49
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-20 12:12:10 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,6 +23,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		printf("%d", x);
 		printf("%s", separator);
+		if (i == n - 1)
+			printf("%d", x);
 	}
 	printf("\n");
 }
