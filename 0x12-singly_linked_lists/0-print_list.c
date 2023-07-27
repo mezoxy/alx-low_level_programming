53f7b3ec1f944af2c192433b40b55bb08191bd7b
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-27 11:22:28 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,14 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
+		if ((h->str))
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
 			printf("[0] (nil)\n");
-		printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		i++;
 	}
