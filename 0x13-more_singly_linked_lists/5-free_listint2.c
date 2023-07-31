8ff00f32d9eaa98c0d7ce017bbc8045190841c8a
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-31 16:51:59 -0700

    Task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - A function that frees a listint_t list.
+ * @head: A pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head)
+	{
+		ptr = *head;
+		*head =(*head)->next;
+		free(ptr);
+	}
+}
