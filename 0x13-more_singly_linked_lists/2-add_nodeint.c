68e4ff1dfe07fb116a0b2e0d21e802fba704edf8
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-07-31 15:30:11 -0700

    Task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_nodeint - A function that add node at the biginningof a list
+ * @h: A pointer
+ * @n: An integer
+ * Return: A pointer
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *ptr = NULL;
+
+	ptr = malloc(sizeof(listint_t));
+	if (!ptr)
+		return (NULL);
+	ptr->next = (*head);
+	*head = ptr;
+	(*head)->n = n;
+	return (*head);
+
+}
