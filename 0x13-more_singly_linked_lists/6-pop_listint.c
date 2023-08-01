6a36860f8579b15a99c268c09f08e9286b9dc843
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-01 08:43:30 -0700

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,2 +1,18 @@
 #include "lists.h"
-#include <>
+#include <stdlib.h>
+/**
+ */
+
+int pop_listint(listint_t **head)
+{
+	int m;
+	listint_t *ptr;
+
+	if (!*head)
+		return (0);
+	ptr = *head;
+	m = (*head)->n;
+	*head = ptr->next;
+	free(ptr);
+	return (m);
+}
