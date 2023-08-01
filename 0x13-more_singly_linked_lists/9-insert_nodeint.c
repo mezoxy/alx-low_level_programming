b1c9d92b6713f77ede12b0db9925858ed8271802
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-01 16:34:20 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (i < idx - 1)
 	{
 		if (!ptr1 || !ptr1->next)
+		{
+			free(ptr);
 			return (NULL);
+		}
 		ptr1 = ptr1->next;
 		i++;
 	}
