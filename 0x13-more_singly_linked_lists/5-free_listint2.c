8bb406967d7d30ae49974698f2c227a60300542a
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-01 08:32:38 -0700

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,11 @@ void free_listint2(listint_t **head)
 	listint_t *ptr;
 
 	ptr = *head;
-	while (*head)
+	while (ptr)
 	{
-		*head = (*head)->next;
+		*head = ptr->next;
 		free(ptr);
 		ptr = *head;
 	}
-	*head = NULL;
+	head = NULL;
 }
