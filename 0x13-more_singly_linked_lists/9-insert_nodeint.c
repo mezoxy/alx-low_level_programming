cc718ab0a524a999a92c4120d22c32a388bca575
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-01 16:30:15 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr, *ptr1 = *head;
-	unsigned int i = 0;
+	unsigned int i;
 
 	ptr = malloc(sizeof(listint_t));
 	if (!ptr)
