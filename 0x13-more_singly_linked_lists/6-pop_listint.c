dab5853aea848193879023eb64594d272cce850e
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-01 08:46:01 -0700

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
+ * pop_listint - A function thatdeletes the head node of a list
+ * @head: A pointer
+ * Return: Head node’s data (n)
  */
 
 int pop_listint(listint_t **head)
