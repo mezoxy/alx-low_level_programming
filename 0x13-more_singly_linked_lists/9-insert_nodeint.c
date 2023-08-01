f77bc988861eacafbef6c554c50e05e160ba3244
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-01 15:38:18 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,48 +1,43 @@
 #include <stdlib.h>
 #include "lists.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
- * insert_nodeint_at_index - Inserts a new node at an idx position
+ * insert_nodeint_at_index - A function
  * @head: A pointer
- * @n: Data to be add
- * @idx: The position
+ * @idx: A position
+ * @n: Data
  * Return: A pointer
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr, *position = *head;
-	unsigned int i = 0;
+	listint_t *ptr, *ptr1;
 
-	while (position)
-	{
-		i++;
-		position = position->next;
-	}
+	if (!*head && idx > 0)
+		return (NULL);
 	ptr = malloc(sizeof(listint_t));
-	if (!ptr || idx > 1 + i)
+	if (!ptr)
 		return (NULL);
 	ptr->n = n;
 	ptr->next = NULL;
 	if (idx == 0)
 	{
-		ptr->next = (*head);
 		*head = ptr;
 		return (*head);
 	}
-	position = *head;
-	while (position)
+	if (idx < listint_len(*head))
 	{
-		if (idx == 1)
-		{
-			ptr->next = position->next;
-			position->next = ptr;
-			return (*head);
-		}
-		position = position->next;
+	ptr1 = *head;
+	idx--;
+	while (idx != 0)
+	{
+		ptr1 = ptr1->next;
 		idx--;
+	}
+	ptr->next = ptr1->next;
+	ptr1 = ptr;
+	return (*head);
 	}
 	return (NULL);
 }
