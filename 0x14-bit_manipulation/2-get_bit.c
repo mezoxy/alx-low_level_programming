95a19230b741249fd439ebf2c88f35acc8f16b77
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-03 16:07:09 -0700

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - A function that determine the value of a bit at a given position
+ * @n: Number
+ * @index: Position
+ * Return: 1 or 0
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	while (index)
+	{
+		n = n >> 1;
+		index--;
+		if (index > 0 && n >> 1 == 0)
+			return (-1);
+	}
+	return (n % 2);
+}
