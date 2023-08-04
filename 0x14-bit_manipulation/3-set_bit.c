8d2d9c4d4ac169d86fabc5d0431dc28f7a4c97d5
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-03 17:35:28 -0700

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,22 +10,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int m = 1, new;
-
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	if (index == 0)
-	{
-		new = *n + 1;
-		*n = new;
-		return (*n);
-	}
-	while (index)
-	{
-		m = m * 2;
-		index--;
-	}
-	new = *n + m;
-	*n = new;
+	*n = *n | (1 << index);
 	return (*n);
 }
