6e387773c5948a14f0856133ba59e5d33a1b6150
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-07 16:47:16 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - A function that reads a text file and print ot to the POSIX
+ * @filename: File to be read and printed
+ * @letters: Num of char to be read and printed
+ * Return: Num of actual number of letters printed and read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fptr;
+	char x;
+	size_t i = 0;
+
+	if (!filename)
+		return (0);
+	fptr = fopen(filename, "r");
+	if (!fptr)
+		return (0);
+	while (i < letters && !feof(fptr))
+	{
+		x = fgetc(fptr);
+		_putchar(x);
+		i++;
+	}
+	fclose(fptr);
+	return (i);
+}
---
+++ 0x15-file_io/REAME.md
@@ -0,0 +1,6 @@
+This directory is for the FILE I/O project :
+
+*Task 0 : Write a function that reads a text file and prints it to the POSIX standard output
+*Task 1 : Create a function that creates a file.
+*Task 2 : Write a function that appends text at the end of a file.
+*task 3 : Write a program that copies the content of a file to another file.
