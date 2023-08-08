3652957edbdc763e5e790782262f703535f86781
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-08 06:49:51 -0700

    Recoding task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,28 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-
+#include <stdio.h>
 /**
  * read_textfile - A function that reads a text file and print ot to the POSIX
  * @filename: File to be read and printed
  * @letters: Num of char to be read and printed
  * Return: Num of actual number of letters printed and read
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *text;
-	ssize_t i, j, k;
+	FILE *ptr;
+	int i = 0;
+	char x;
 
-	i = open(filename, O_RDONLY);
-	if (i == -1)
+	if (!filename)
 		return (0);
-	text = malloc(sizeof(char) * letters);
-	if (!text)
+	ptr = fopen(filename, "r");
+	if (!ptr)
 		return (0);
-	j = read(i, text, letters);
-	k = write(STDOUT_FILENO, text, j);
-	free(text);
-	close(i);
-	return (k);
+	while ((x = getc(ptr)) != EOF && i < (int)letters)
+	{
+		putchar(x);
+		i++;
+	}
+	return (i);
+
 }
