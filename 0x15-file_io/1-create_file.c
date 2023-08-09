8735be62ac73908b9fc46a73cdff3ebaea5c8028
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-09 14:28:39 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		close(j);
 		return (1);
 	}
-	i = (int)open(filename, O_WRONLY | O_TRUNC);
+	i = (int)open(filename, O_WRONLY | O_TRUNC | O_CREAT);
 	if (i == -1)
 		return (-1);
 	h = (int)write(i, text_content, strlen(text_content));
