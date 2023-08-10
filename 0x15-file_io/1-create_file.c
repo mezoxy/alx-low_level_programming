3ee462d6233dd01e9e898f02f548361423f389e2
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-08-10 03:38:39 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,28 +14,19 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i, j, h, d;
+	int i, j, y = 0;
 
 	if (filename == NULL)
 		return (-1);
-	d = (int)open(filename, O_RDONLY);
-	if (d == -1)
-	{
-		j = (int)open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-		h = (int)write(j, text_content, strlen(text_content));
-		if (h == -1)
-		{
-			close(j);
-			return (-1);
-		}
-		close(j);
-		return (1);
-	}
-	i = (int)open(filename, O_WRONLY | O_TRUNC | O_CREAT);
+	if (!text_content)
+		text_content = "";
+	while (text_content[y] != '\0')
+		y++;
+	i = (int)open(filename, O_CREAT | O_WRONLY | O_CREAT, 0600);
 	if (i == -1)
 		return (-1);
-	h = (int)write(i, text_content, strlen(text_content));
-	if (h == -1)
+	j = (int)write(i, text_content, y);
+	if (j == -1)
 	{
 		close(i);
 		return (-1);
