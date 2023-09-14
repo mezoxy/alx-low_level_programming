41d37bed53060cf2943e8e65f914aca3a8361dc7
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-14 12:11:00 -0700

    gf

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -26,8 +26,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
-	new->prev = NULL;
 	new->next = *head;
+	(*head)->prev = new;
+	new->prev = NULL;
 	*head = new;
 	return (*head);
 }
