59f6ce4bf764fe7a9d66dba45f9c62e9c4e930ec
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-14 12:06:20 -0700

    gf

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,16 +11,23 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
 
+	if (!*head)
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (!new)
+			return (NULL);
+		new->n = n;
+		new->prev = NULL;
+		new->next = NULL;
+		*head = new;
+		return (*head);
+	}
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	if (!*head)
-	{
-		new->next = NULL;
-		*head = new;
-	}
-	*head->next = new;
+	new->next = *head;
+	*head = new;
 	return (*head);
 }
