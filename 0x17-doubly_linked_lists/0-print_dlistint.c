499bc1ef81f056f6567b5c751f4569c9a5762f2c
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-14 03:50:14 -0700

    Task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - A function that  all the elements of a list
+ * @h: A pointer
+ * Return: Number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	if (!h)
+		return (0);
+	i = 0;
+	while (h)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
