95692efbbbe66ff54981b5305bee11bfd8631f71
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-14 12:55:10 -0700

    Task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,13 +1,16 @@
 #include "lists.h"
 
 /**
+ * sum_dlistint - A function
+ * @head: A pointer
+ * Return: Sum of n
  */
 
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
-
 	dlistint_t *p = head;
+
 	while (p)
 	{
 		sum += p->n;
