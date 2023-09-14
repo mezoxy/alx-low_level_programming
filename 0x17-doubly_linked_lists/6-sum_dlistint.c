dd4e535c9e7f5144ef23ae605a0c29cfe6743cf9
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-14 12:52:36 -0700

    Task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *p = head;
+
+	while (p)
+	{
+		sum += p->n;
+		p = p->next;
+	}
+	return (sum);
+}
