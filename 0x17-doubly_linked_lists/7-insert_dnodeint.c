b388f7c68e5bbffd7a29cc284e5a9f83dc275d4c
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-16 09:00:23 -0700

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	i = 0;
-	while (tmp && *h && idx != 0)
+	while (tmp && *h)
 	{
 		if (idx == i)
 		{
