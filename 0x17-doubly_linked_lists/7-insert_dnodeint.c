5edc9aacb71624a0eb52299183b9e74ce1ed5633
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-17 08:22:10 -0700

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,33 +7,36 @@
  * @n: Data
  * Return: A node
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new;
-
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
-	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
-	}
-
-	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
+	dlistint_t *new, *tmp = *h;
+	unsigned int i;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
-
 	new->n = n;
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp->next->prev = new;
-	tmp->next = new;
-
-	return (new);
+	if (idx == 0)
+	{ *h = add_dnodeint(h, n);
+		return (*h); }
+	i = 0;
+	while (tmp)
+	{
+		if (idx == i)
+		{
+			new->next = tmp;
+			new->prev = tmp->prev;
+			tmp->prev->next = new;
+			tmp->prev = new;
+			return (*h);
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	if (idx == i + 1)
+	{
+		*h = add_dnodeint_end(h, n);
+		return (*h);
+	}
+	return (NULL);
 }
