4c9eeb3b6cdf2c6f83886cffd822186f5fae64e3
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-17 07:09:21 -0700

    Task 8

--- 0x17-doubly_linked_lists/del.c
+++ 0x17-doubly_linked_lists/del.c
@@ -1,13 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a
- * dlistint_t linked list
- *
- * @head: A pointer
- * @index: An index
- * Return: 1 if it succeeded, else -1
  */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *del = *head;
