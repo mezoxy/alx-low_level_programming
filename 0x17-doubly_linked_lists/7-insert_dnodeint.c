09d68a25202abc764005d74b0f3255c26b309d7a
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-16 18:35:17 -0700

    Task 77

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,29 +7,31 @@
  * @n: Data
  * Return: A node
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *tmp = *h;
+	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	while (idx != 1)
+
+	for (; idx != 1; idx--)
 	{
 		tmp = tmp->next;
-		idx--;
-		if (!tmp)
-			return (NULL); }
+		if (tmp == NULL)
+			return (NULL);
+	}
 
-	if (!tmp->next)
+	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 
-	new->next = tmp->next;
 	new->n = n;
 	new->prev = tmp;
+	new->next = tmp->next;
 	tmp->next->prev = new;
 	tmp->next = new;
 
