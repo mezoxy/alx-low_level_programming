9d82b11e05d46acf8ab2cb7de60e5be7541ab285
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-18 07:28:36 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/opertion.c
@@ -0,0 +1,61 @@
+#include "op.h"
+#include <stdio.h>
+/**
+ * add - A function that calculates the sum of 2 num
+ * @a: An int
+ * @b: An int
+ * Return: The div
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - A function that calculates the sub of 2 num
+ * @a: An int
+ * @b: An int
+ * Return: The div
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - A function that calculates the mul of 2 num
+ * @a: An int
+ * @b: An int
+ * Return: The div
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * rem - A function that calculates remainder
+ * @a: An int
+ * @b: An int
+ * Return: The div
+*/
+
+int rem(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * div - A function that calculates div
+ * @a: An int
+ * @b: An int
+ * Return: The div
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
