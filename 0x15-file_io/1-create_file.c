1d75fdea58fc97339c81ec5e7a5600b2699313d5
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-21 15:30:52 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
+ * create_file - A function that creates a file.
+ * @filename: file stream
+ * @text_content: A string
+ * Return: 1 if success else -1
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t w;
 
+	if (!filename)
+		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
