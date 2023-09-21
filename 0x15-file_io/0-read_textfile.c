df90428ae596905789fe5e9503c63894e8155a8a
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-21 14:29:45 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * read_textfile - Afunction that reads a text file and prints it to stdout
+ * @filename: File stream
+ * @letters: Num of char
+ * Return: The actual number of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -15,7 +19,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
 	r = read(fd, buf, letters);
