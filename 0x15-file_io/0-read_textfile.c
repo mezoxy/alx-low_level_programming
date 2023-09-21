f9b6f1eaaa6979673b7c5c4df1293052032fc13c
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-21 14:22:36 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,6 +12,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(sizeof(char) * (letters + 1));
 	if (!buf)
 		return (0);
+	if (!filename)
+		return (0);
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
