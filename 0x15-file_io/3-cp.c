e7fbcb4435e5728d6f2412bd3ae09d364ab16372
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-21 18:21:55 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,18 +7,18 @@ int main(int argc, char **argv)
 {
 	int fd_from, fd_to, i, j;
 	ssize_t w, r;
-	char *buf[1024];
+	char buf[2000];
 
 	if (argc != 3)
 	{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); }
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	fd_from = open(argv[1], O_RDONLY);
 	r = read(fd_from, buf, 1024);
 	if (fd_from == -1 || r == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	w = write(fd_to, buf, r);
 	if (fd_to == -1 || w == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
