814336764607374e335aab17be59c4e6f055fb9f
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-09-21 17:19:58 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,101 +1,35 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
 /**
- * main - Entry point
- * @argc: Num of arg
- * @argv: A ponter
- * Return: 0 or 1
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-	int from, to, r, w;
-	char *buffer;
+	int fd_from, fd_to, i, j;
+	ssize_t w, r;
+	char *buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97); }
+	fd_from = open(argv[1], O_RDONLY);
+	r = read(fd_from, buf, 1024);
+	if (fd_from == -1 || r == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	do {
-		if (from == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
-
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (r > 0);
-
-	free(buffer);
-	close_file(from);
-	close_file(to);
-
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	w = write(fd_to, buf, r);
+	if (fd_to == -1 || w == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
+	i = close(fd_from);
+	j = close(fd_to);
+	if (j == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_to);
+		exit(100); }
+	if (i == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_from);
+		exit(100); }
 	return (0);
-
-}
-
-/**
- * close_file - A function
- * @fd: An int
- *
- */
-
-void close_file(int fd)
-{
-	int c;
-
-	c = close(fd);
-
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-
-
-/**
- * create_buffer - A function
- * @file: A pointer
- * Return: A char
- */
-
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
