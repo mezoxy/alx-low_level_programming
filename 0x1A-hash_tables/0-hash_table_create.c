ed4de17ef6a9ad8420ec4600d95592c33ec40d26
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-05 09:34:52 -0700

    Task 0 and 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,14 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_create - A function that creates a hash table
+ * @size: Size of the hash table
+ * Return: A pointer
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *our_table = NULL;
-	/*unsigned long int i;*/
 
 	if (size == 0)
 		return (NULL);
@@ -23,7 +25,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(our_table);
 		return (NULL);
 	}
-	/**for (i = 0 ; i < size; i++)
-		our_table->array[i] = NULL;*/
 	return (our_table);
 }
