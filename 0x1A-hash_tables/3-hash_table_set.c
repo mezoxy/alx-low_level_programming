5a11555221e1b52e4047426491c37452912ff46d
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-05 16:51:09 -0700

    Task 0 changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!node2add->value || !node2add->key)
 	{
 		free(node2add->value);
-		free(node2add->key)
+		free(node2add->key);
 		free(node2add);
 		return (0);
 	}
