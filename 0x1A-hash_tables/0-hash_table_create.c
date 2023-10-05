c6570c34c27eb9464dba932eca09c4dc33fec02f
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-05 09:27:03 -0700

    Task 0 and 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,12 +18,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	our_table->size = size;
 	our_table->array = calloc(size, sizeof(hash_node_t *));
 
-	/**if (!our_table->array)
+	if (!our_table->array)
 	{
 		free(our_table);
 		return (NULL);
 	}
-	for (i = 0 ; i < size; i++)
+	/**for (i = 0 ; i < size; i++)
 		our_table->array[i] = NULL;*/
 	return (our_table);
 }
