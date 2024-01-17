85ead44bade7642c8e91cfbeb2e093c17c3c3a64
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-16 20:14:44 -0800

    Task 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -40,15 +40,5 @@ int jump_search(int *array, size_t size, int value)
 			}
 		}
 	}
-	if (i - jump < (int)size)
-	{
-		printf("Value found between indexes [%d] and [%d]\n", i - jump, tmp);
-		for (j = i - jump; j < (int)size; j++)
-		{
-			printf("Value checked array[%d] = [%d]\n", j, array[j]);
-			if (value == array[j])
-				return (j);
-		}
-	}
 	return (-1);
 }
